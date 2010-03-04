dae34900b22db7787c68fe2c37e57383a85d7624
César D. Rodas <crodas@php.net>
2010-03-04 08:38:28 -0300

    - Added copyright headers - Fixded little issue at the session example

--- samples/sessions/example.php
+++ samples/sessions/example.php
@@ -1,4 +1,20 @@
 <?php
+/*
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2009 The PHP Group                                     |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Cesar Rodas <crodas@php.net>                                |
+  +----------------------------------------------------------------------+
+*/
+
 require "../../ActiveMongo.php";
 require "sessions.php";
 
@@ -9,5 +25,8 @@ MongoSession::init();
 
 session_start();
 var_dump(array('previous data' => $_SESSION));
+if (!isset($_SESSION['data'])) {
+    $_SESSION['data'] = 0;
+}
 $_SESSION['data']++;
 var_dump(array('current data' => $_SESSION));
